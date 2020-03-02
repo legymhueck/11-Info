3dea542f3dfe799c27fa100e7e8c71dbf783e010
legymhueck <lehnen@gymnasium-hueckelhoven.de>
2020-03-02 11:28:06 +0100

    ZaehlenVonBis

---
+++ src/lehnen/_05Rekursion/_08Uebung_20200302/ZaehlenVonBis.java
@@ -0,0 +1,26 @@
+/**
+ * Hier geht es darum, das Zählen von einer ersten bis zu einer letzen Zahl
+ * iterativ und rekursiv zu implementieren.
+ *
+ * Beim iterativen Ansatz würde man üblicherweise eine Schleife (while oder for) benutzen.
+ *
+ * Beim rekursiven Ansatz ist immer Folgendes zu bedenken:
+ *  - Wie lautet der Basisfall?
+ *  - Was soll sonst geschehen, d. h. wenn der Basisfall nicht zutrifft?
+ *
+ * Implementiere den iterativen und den relursiven Ansatz!
+ */
+package lehnen._05Rekursion._08Uebung_20200302;
+
+public class ZaehlenVonBis {
+    public static void main(String[] args) {
+        ZaehlenVonBis zaehlenVonBis = new ZaehlenVonBis();
+
+        // Wenn du die Methoden summeIterativ bzw. summeRekursiv geschrieben hast, kannst du die folgenden Zeilen auskommentieren.
+
+        //System.out.println("Iterativ: " + zaehlenVonBis.summeIterativ(0, 3));
+        //System.out.println("Rekursiv: " + zaehlenVonBis.summeRekursiv(0, 3));
+    }
+
+    // Hierhin gehören deine beiden Methoden:
+}
