113c357d267eb1542355aac3002a033058376666
legymhueck <lehnen@gymnasium-hueckelhoven.de>
2020-05-06 11:15:57 +0200

    Tiefe, Knoten, Blattzahl bestimmen

---
+++ src/lehnen/_08NichtLineareDatenstrukturen/_04Tiefe/TiefeBestimmen.java
@@ -0,0 +1,5 @@
+package lehnen._08NichtLineareDatenstrukturen._04Tiefe;
+
+public class TiefeBestimmen {
+
+}
---
+++ src/lehnen/_08NichtLineareDatenstrukturen/_05Knotenzahl/KnotenzahlBestimmen.java
@@ -0,0 +1,5 @@
+package lehnen._08NichtLineareDatenstrukturen._05Knotenzahl;
+
+public class KnotenzahlBestimmen {
+
+}
